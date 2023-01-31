9c530a9a897a278e736b4b9c22b4f1404ee0e531
Chris Owen <chris@sigmalabs.xyz>
2023-01-31 17:31:02 +0000

    PR fixes

--- blackjack/blackjack_oop.py
+++ blackjack/blackjack_oop.py
@@ -1,20 +1,20 @@
 class Card:
-    def __init__(self, rank, suit):
+    def __init__(self, rank: str, suit: str) -> None:
         self.rank = rank
         self.suit = suit
 
-    def to_string(self):
-        # TODO Complete this method so that it returns the correct string to represent the hand
+    def to_string(self) -> str:
+        """Returns the correct string to represent the hand"""
         return ''
 
-    @ property
-    def points(self):
-        # TODO Complete this method so that it gets the correct number of points for a card
+    @property
+    def points(self) -> int:
+        """Gets the correct number of points for a card"""
         return 0
 
 
 class Hand:
-    def __init__(self, cards):
+    def __init__(self, cards: list) -> None:
         all_cards = all(isinstance(card, Card) for card in cards)
 
         if (not all_cards):
@@ -23,19 +23,20 @@ class Hand:
         self.cards = cards
 
     @property
-    def points(self):
-        # TODO Complete this method so that it calculates the points for a hand of cards
+    def points(self) -> int:
+        """Calculates the points for a hand of cards"""
         return 0
 
 
 class Deck:
-    def __init__(self):
+    def __init__(self) -> None:
         # TODO Complete this method so the deck contains all the correct cards
         self.cards = []
 
-    def draw(self):
+    def draw(self) -> Card:
+        """Removes and returns the top card from the deck"""
         return self.cards.pop(0)
 
-    def shuffle(self):
-        # TODO Complete this method so that it shuffles the deck
+    def shuffle(self) -> None:
+        """Shuffles the cards in this deck"""
         print(self.cards)
