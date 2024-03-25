092e5ea5a1b8f30f3f0887257713c00344c30299
Faris Abulula <abululb02@gmail.com>
2024-03-25 13:12:02 +0000

    blockbuster video

--- blockbuster/blockbuster_oop.py
+++ blockbuster/blockbuster_oop.py
@@ -1,6 +1,32 @@
+from datetime import datetime
+
+
 class Video:
-    def __init__(self):
+    def __init__(self, title: str, year: int, runtime: int) -> None:
         print("Video Init")
+        if (year < 1900):
+            raise Exception("Videos can't be released before 1900")
+        if title == None:
+            raise Exception("Title most be provided")
+        self.title = title
+        self.year = year
+        self.runtime = runtime
+
+    def display_title(self):
+        return f"{self.title} ({self.year})"
+
+    def rental_price(self):
+        if self.year == datetime.now().year and self.runtime > 240:
+            return 2000
+        if self.year == datetime.now().year and self.runtime <= 240:
+            return 1000
+        if self.runtime > 240:
+            return 1000
+        return 500
+
+    def display_price(self):
+        price = str(self.rental_price())
+        return (f"£{price[:-2]}.{price[-2:]}")
 
 
 class Customer:
