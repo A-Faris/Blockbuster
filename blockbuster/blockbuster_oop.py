8e06e352663d8a08fdca5206d5a7b4b0e716dc24
Faris Abulula <abululb02@gmail.com>
2024-03-25 14:21:20 +0000

    codeing problem done

--- blockbuster/blockbuster_oop.py
+++ blockbuster/blockbuster_oop.py
@@ -1,21 +1,27 @@
+"""Blockbuster"""
+
 from datetime import datetime
 
 
 class Video:
+    """Video class"""
+
     def __init__(self, title: str, year: int, runtime: int) -> None:
         print("Video Init")
-        if (year < 1900):
+        if year < 1900:
             raise Exception("Videos can't be released before 1900")
-        if title == None:
+        if title is None:
             raise Exception("Title most be provided")
         self.title = title
         self.year = year
         self.runtime = runtime
 
     def display_title(self):
+        """Displays title"""
         return f"{self.title} ({self.year})"
 
     def rental_price(self):
+        """Calculates rental price"""
         if self.year == datetime.now().year and self.runtime > 240:
             return 2000
         if self.year == datetime.now().year and self.runtime <= 240:
@@ -25,30 +31,41 @@ class Video:
         return 500
 
     def display_price(self):
+        """Displays the price"""
         price = str(self.rental_price())
-        return (f"£{price[:-2]}.{price[-2:]}")
+        return f"£{price[:-2]}.{price[-2:]}"
 
 
 class Customer:
+    """Customer"""
+
     def __init__(self):
         print("Customer Init")
 
 
 class VideoStore:
+    """Video Store"""
+
     def __init__(self):
         print("VideoStore Init")
 
 
 class Rental:
+    """Rental"""
+
     def __init__(self):
         print("Rental Init")
 
 
 class DVD(Video):
+    """DVD"""
+
     def __init__(self):
         print("DVD Init")
 
 
 class VendingMachine(VideoStore):
+    """Vending Machine"""
+
     def __init__(self):
         print("VendingMachine Init")
