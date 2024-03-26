441f17ee689072dec58c42b0313d0224010ba311
Faris Abulula <abululb02@gmail.com>
2024-03-26 13:26:57 +0000

    VideoStore

--- blockbuster/blockbuster_oop.py
+++ blockbuster/blockbuster_oop.py
@@ -1,6 +1,6 @@
 """Blockbuster"""
 
-from datetime import datetime
+import datetime
 
 
 class Video:
@@ -9,28 +9,29 @@ class Video:
     def __init__(self, title: str, year: int, runtime: int) -> None:
         print("Video Init")
         if year < 1900:
-            raise Exception("Videos can't be released before 1900")
+            raise ValueError("Videos can't be released before 1900")
         if title is None:
-            raise Exception("Title most be provided")
+            raise ValueError("Title must be provided")
         self.title = title
         self.year = year
         self.runtime = runtime
 
-    def display_title(self):
+    def display_title(self) -> str:
         """Displays title"""
         return f"{self.title} ({self.year})"
 
-    def rental_price(self):
+    def rental_price(self) -> int:
         """Calculates rental price"""
-        if self.year == datetime.now().year and self.runtime > 240:
-            return 2000
-        if self.year == datetime.now().year and self.runtime <= 240:
-            return 1000
+        price = 0
+        if self.year == datetime.now().year:
+            price = 1000
+        else:
+            price = 500
         if self.runtime > 240:
-            return 1000
-        return 500
+            return price * 2
+        return price
 
-    def display_price(self):
+    def display_price(self) -> str:
         """Displays the price"""
         price = str(self.rental_price())
         return f"£{price[:-2]}.{price[-2:]}"
@@ -44,23 +45,45 @@ class Customer:
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
+        self.age()
 
     @property
     def name(self) -> str:
+        """Shows name"""
         return f"{self.first_name} {self.last_name}"
 
-    def age(self, number: int) -> int:
-        try:
-            return int(number)
-        except:
+    def age(self, age=None) -> int:
+        """Calculates age"""
+        if age is None:
+            day, month, year = map(int, self.date_of_birth.split("/"))
+            today = datetime.date.today()
+            age = today.year - year - ((today.month, today.day) < (month, day))
+
+        if age < 13:
             raise ValueError
+
+        try:
+            return age
+        except Exception as exc:
+            raise ValueError("Must be an integer") from exc
 
 
 class VideoStore:
     """Video Store"""
 
-    def __init__(self):
+    def __init__(self, videos) -> None:
         print("VideoStore Init")
+        if 0 < len(videos) < 10:
+            self.videos = videos
+        else:
+            raise ValueError
+
+    def find_video_by_title(self, title: str) -> Video:
+        """Show title"""
+        for video in self.videos:
+            if title in video.display_title():
+                return video.display_title()
+        raise ValueError("This video does not exist")
 
 
 class Rental:
--- blockbuster/test_blockbuster_trainee.py
+++ blockbuster/test_blockbuster_trainee.py
@@ -24,8 +24,39 @@ def test_date_of_birth(john_smith):
 
 
 def test_age(john_smith):
-    john_smith.age(34) == 34
+    john_smith.age() == 34
 
 
 def test_age_float(john_smith):
     john_smith.age(34.5) == ValueError
+
+
+def test_age_young():
+    with pytest.raises(Exception):
+        Customer("Jimmy", "Jimmy", '24/01/2020')
+
+
+def test_age_young_second(john_smith):
+    with pytest.raises(Exception):
+        john_smith.age(12)
+
+
+@pytest.fixture
+def video():
+    matrix = Video('The Matrix', 1999, 150)
+    terminator = Video('The Terminator', 1985, 108)
+    return VideoStore([matrix, terminator])
+
+
+def test_VideoStore_no_videos():
+    with pytest.raises(Exception):
+        VideoStore([])
+
+
+def test_display_title(video):
+    video.find_video_by_title('The Matrix') == 'The Matrix'
+
+
+def test_display_title_not_real(video):
+    with pytest.raises(Exception):
+        video.find_video_by_title('Not here')
